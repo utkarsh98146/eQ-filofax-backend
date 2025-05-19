662a02967c0e0b3c8a185bd3ee636eb8ed19e201
utkarsh98146 <subhashyadav.equasar@gmail.com>
2025-05-19 15:38:15 +0530

    work on zoom integration

--- src/utils/generateZoomToken.utils.js
+++ src/utils/generateZoomToken.utils.js
@@ -3,7 +3,13 @@ import { zoomApiConfig } from "../config/zoomApi.config.js"
 export const generateZoomToken = async () => {
   try {
     const { zoomClientId, zoomAccountId, zoomClientSecret } = zoomApiConfig()
-    const encodedCredentials = base64.encode(`${zoomAccountId}`)
+    const encodedCredentials = base64.encode(
+      `${zoomClientId}:${zoomClientSecret}`
+    )
+    return {
+      Authorization: `Basic ${encodedCredentials}`,
+      "Content-Type": "application/json",
+    }
   } catch (error) {
     console.error("Error generating Zoom token:", error)
     throw new Error("Failed to generate Zoom token")
